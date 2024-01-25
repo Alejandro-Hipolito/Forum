9744557c695d96a1e10a8521278ab45273deb64b
Alejandro Hipólito Hernando <alejandroohipolito@gmail.com>
2024-01-25 23:13:57 +0100

    bootstrap + navbar setup added

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import Contact from "./Contact"
 // import Menu from "./menu" 
 import Menu from './menu'
 import NOTFOUND from "./not_found"
+import "./navbar.css"
 
 function App() {
   return (
-    <div>
+    <div className="" style={{marginTop: '6rem', marginLeft: '3rem', marginRight: '3rem'}}>
+    {/* // <div className="NavBar">
 
-      <h1>Routes</h1>
+    //   <h1>Routes</h1> */}
       <Routes>
         <Route path="/" element={<Layout/>} >
           <Route path="/menu" element={<Menu/>}></Route>
--- client/src/Layout.js
+++ client/src/Layout.js
@@ -1,29 +1,83 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import { Outlet } from "react-router-dom"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import Container from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 function Layout() {
   return (
-    <div>
-
-      <nav>
-        <ul>
-          <li>
-            <Link to="/menu">Menu</Link>
-          </li>
-
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-
-
-        </ul>
+    <div className='mx-auto'>
+<nav className="navbar navbar-expand-lg bg-body-tertiary justify-content-center position-absolute top-0 start-50 translate-middle-x">
+        <div className="container-fluid mx-auto">
+          <Link className="navbar-brand " to="/" style={{marginRight: '10rem'}}>
+            Navbar
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link active " to="/menu" style={{marginRight: '10rem'}}>
+                  Menu
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link " to="/contact" style={{marginRight: '10rem'}}>
+                  Contact
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link " to="/pricing" style={{marginRight: '10rem'}}>
+                  Pricing
+                </Link>
+              </li>
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle "
+                  to="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown link
+                </Link>
+                <ul className="dropdown-menu">
+                  <li>
+                    <Link className="dropdown-item" to="#">
+                      Action
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="#">
+                      Another action
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="#">
+                      Something else here
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
 
-    <hr></hr>
-      <Outlet/>
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
